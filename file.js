84ad114b17a28b377d71243f9c71d6dca805f2fa
Julio Cardona <jucardona71599@gmail.com>
2022-01-14 00:00:43 -0800

    create initial grid

--- file.js
+++ file.js
@@ -7,3 +7,11 @@ function grid() {
 for (let i=0; i<256; i++) {
 grid()
 }
+//create divs
+function grid() {
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+    container.appendChild(grid);}
+for (let i=0; i<256; i++) {
+grid()
+}
