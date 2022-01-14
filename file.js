bd85e1d2e666f151b1eb4828598bf8d567bdb0aa
Julio Cardona <jucardona71599@gmail.com>
2022-01-14 00:14:20 -0800

    adjusted grid layout to incorporate user selection, etch-a-sketch complete

--- file.js
+++ file.js
@@ -7,24 +7,26 @@ function grid() {
 for (let i=0; i<256; i++) {
 grid()
 }
-//create divs
-function grid() {
-    const grid = document.createElement("div");
-    grid.classList.add("grid");
-    container.appendChild(grid);}
-for (let i=0; i<256; i++) {
-grid()
-}
+const divs = document.querySelectorAll("div.grid");
+function divwidth() {
+const divs = document.querySelectorAll("div.grid");
+let number = Math.sqrt(document.getElementsByClassName("grid").length)
+let dwidth = ((960-(5*number))/number) + "px"
+divs.forEach((div) => { div.style.width = dwidth
+});}
+divwidth()
 //turn blue when mouseover
 function color() {
-    const divs = document.querySelectorAll("div.grid");
-    divs.forEach((div) => {
-    div.addEventListener("mouseover", () => { 
-    div.setAttribute("style", "background-color:blue;");
-    });
-    });}
-    color()
-//create restart button and divs of new grid
+const divs = document.querySelectorAll("div.grid");
+divs.forEach((div) => {
+div.addEventListener("mouseover", () => { 
+div.setAttribute("style", "background-color:blue;");
+divwidth()
+});
+});}
+color()
+
+//create restart button
 const btn= document.querySelector("button");
 btn.addEventListener( 'click', () => {
 document.querySelectorAll('.grid').forEach(e => e.remove());
@@ -35,4 +37,6 @@ console.log(sizen)
 let gridsize = Math.pow(sizen,2)
 for (let i=0; i<gridsize; i++) {
     grid()};
+divwidth();
+color()
 });
